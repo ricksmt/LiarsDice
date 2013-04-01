f22a6a577c52fb41c52b548ee61322b082236b7f
Matthew Ricks <matthew@sixricks.com>
2013-04-01 15:46:29 -0600

    Working on a better bot.

--- LiarsDice/src/model/liarsDice/bots/TRicks.java
+++ LiarsDice/src/model/liarsDice/bots/TRicks.java
@@ -19,16 +19,17 @@ import model.liarsDice.gameLogic.LiarsDiceBot;
  */
 public class TRicks extends LiarsDiceBot {
 	
-	private static int STRETCH = 1;// I'm willing to believe STRETCH more than the average.
+	private static double TRUST = 0.4;
 	private Random random = new Random();
+	private static int GAME_COUNT = 0;
+	private static int WIN_COUNT = 0;
+	private static int TOP_TWO = 0;
 
-	public String getName() {
-		return "TRicks";
-	}
+	public String getName() { return "TRicks"; }
 
 	public Decision getDecision(GameInfo currentGameInfo) {
-		Bid currentBid = currentGameInfo.getCurrentBid();
-		if(currentBid == null) return calculateFirstBid(currentGameInfo);
+		if(currentGameInfo.getCurrentBid() == null)
+			return calculateFirstBid(currentGameInfo);
 		else return makeDecision(currentGameInfo);
 	}
 
@@ -44,9 +45,9 @@ public class TRicks extends LiarsDiceBot {
 		int othersDice = totalDice - currentGameInfo.getMyDice().size();
 		int onesGuess = othersDice/6 + myDiceFrequencies[0];
 		int bidFrequencyGuess = myDiceFrequencies[currentBid.getFaceValue() - 1] + othersDice/6;
-		if(currentBid.getFrequency() > (onesGuess + bidFrequencyGuess + STRETCH)) return new Challenge();
+		if(currentBid.getFrequency() >
+			(onesGuess + bidFrequencyGuess + Math.log((double)totalDice / 6))) return new Challenge();
 		else if(currentBid.getFrequency() < (onesGuess + bidFrequencyGuess)){
-			
 			if(currentBid.getFaceValue() == 6)
 				return new Bid(currentBid.getFrequency() + 1, currentBid.getFaceValue());
 			else return new Bid(currentBid.getFrequency(), currentBid.getFaceValue() + 1);
@@ -55,7 +56,8 @@ public class TRicks extends LiarsDiceBot {
 			List<Round> rounds = currentGameInfo.getGameHistory().getRounds();
 			List<Turn> turns = rounds.get(rounds.size() - 1).getTurns();
 			int opponent = turns.get(turns.size() - 1).getPlayerID();
-			int veracity = 0, count = 0;
+			double veracity = 0;
+			int count = 0;
 			for(Round round: rounds){
 				switch(round.getResult()){
 					case WINNING_CHALLENGE:
@@ -74,17 +76,32 @@ public class TRicks extends LiarsDiceBot {
 						break;
 				}
 			}
-			if(veracity < 0) return new Challenge();// Liar?!?
+			veracity = ((double)(count / 2 + veracity)/(double)count);
+			if(veracity < TRUST) return new Challenge();// Liar?!?
 			
 			// Now I'm gonna make them freak out.
 			int high = 5;
-			for(int i = 0; i < 5; i++) if(myDiceFrequencies[i] > myDiceFrequencies[5]) high = i;
-			if(high > currentBid.getFaceValue()) return new Bid(currentBid.getFrequency(), high);
-			else if(currentBid.getFaceValue() != 6) return new Bid(currentBid.getFrequency(), 6);
-			if(((double)(count / 2 + veracity)/(double)count) > random.nextDouble())
-				return new Bid(currentBid.getFrequency() + 1, currentBid.getFaceValue());
-			else return new Bid(currentBid.getFrequency() + 1,
-				random.nextBoolean() ? high : random.nextInt(5) + 2);
+			for(int i = 0; i < 5; i++) if(myDiceFrequencies[i] > myDiceFrequencies[high]) high = i;
+			high++;
+			if(high > currentBid.getFaceValue())
+				return finalDecision(new Bid(currentBid.getFrequency(), high), currentGameInfo);
+			else if(high == currentBid.getFaceValue())
+				return finalDecision(new Bid(currentBid.getFrequency() + 1, high), currentGameInfo);
+			else if(veracity > random.nextDouble())
+				return finalDecision(new Bid(currentBid.getFrequency() + 1, currentBid.getFaceValue()), currentGameInfo);
+			else return finalDecision(new Bid(currentBid.getFrequency() + 1,
+				random.nextBoolean() ? high : random.nextInt(5) + 2), currentGameInfo);
+		}
+	}
+	
+	private Decision finalDecision(Decision decision, GameInfo currentGameInfo){
+		if(checkValidDecision(decision, currentGameInfo)){
+//			System.out.println(currentGameInfo.getTotalDice() + " My decision is final.");
+			return decision;
+		}
+		else{
+//			System.out.println(currentGameInfo.getTotalDice() + " You're fired.");
+			return new Challenge();
 		}
 	}
 
@@ -100,6 +117,20 @@ public class TRicks extends LiarsDiceBot {
 	}
 
 	public void reportRoundResults(GameInfo gameInfo){
-		if(gameInfo.getMyPlayerID() == gameInfo.getWinnerID()) System.out.println("I won!!!");
+		GAME_COUNT++;
+		if(gameInfo.getMyPlayerID() == gameInfo.getWinnerID()) WIN_COUNT++;
+		else{
+			List<Round> rounds = gameInfo.getGameHistory().getRounds();
+			for(int i = rounds.size() - 1; i >= 0; i--){
+				for(Turn turn: rounds.get(i).getTurns()){
+					if(turn.getPlayerID() == gameInfo.getMyPlayerID()){
+						if(i + 1 == rounds.size()){
+							TOP_TWO++;
+						}
+						break;
+					}
+				}
+			}
+		}
 	}
 }
